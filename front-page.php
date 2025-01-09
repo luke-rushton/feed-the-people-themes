2281be508f4c1abab8aa9a72719fbd9aa8461f3e
luke-rushton <luke.rushtonx@gmail.com>
2025-01-08 16:27:26 -0800

    committing theme structure

---
+++ front-page.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Feed_The_People_Themes
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<!-- check for ACF -->
+			<?php if (function_exists('the_field')): ?>
+
+				<!-- Splash -->
+				<?php if (get_field('page_title')): ?>
+					<h1><?php the_field('page_title') ?></h1>
+					<?php endif; ?>
+
+				<?php if (get_field('page_subtitle')): ?>
+					<p><?php the_field('page_subtitle') ?></p>
+					<?php endif; ?>
+
+				<?php if (have_rows('page_menu_items')): ?>
+					<ul>
+						<?php while (have_rows('page_menu_items')): the_row() ?>
+							<li><a><?php echo get_sub_field('section_title') ?></a></li>
+						<?php endwhile; ?>
+					</ul>
+				<?php endif; ?>
+
+				<!-- Works -->
+				<?php if (get_field('works_title')): ?>
+					<h2><?php the_field('works_title') ?></h2>
+					<?php endif; ?>
+
+				<?php if (have_rows('works')): ?>
+					<ul>
+						<?php while (have_rows('works')): the_row() ?>
+							<li><a><?php echo get_sub_field('work_title') ?></a></li>
+						<?php endwhile; ?>
+					</ul>
+				<?php endif; ?>
+
+				<!-- About -->
+				<?php if (get_field('about_title')): ?>
+					<h2><?php the_field('about_title') ?></h2>
+					<?php endif; ?>
+
+				<?php if (get_field('about_description')): ?>
+					<p><?php the_field('about_description') ?></p>
+					<?php endif; ?>
+
+				<!-- Contact CTA -->
+				<?php if (get_field('contact_title')): ?>
+					<h2><?php the_field('contact_title') ?></h2>
+					<?php endif; ?>
+
+				<?php if (get_field('button_text')): ?>
+					<p><?php the_field('button_text') ?></p>
+					<?php endif; ?>
+
+				<!-- Team Section -->	
+				<?php if (get_field('team_title')): ?>
+					<h2><?php the_field('team_title') ?></h2>
+					<?php endif; ?>
+
+				<?php if (get_field('team_description')): ?>
+					<p><?php the_field('team_description') ?></p>
+					<?php endif; ?>
+
+				<?php if (have_rows('team_members')): ?>
+					<ul>
+						<?php while (have_rows('team_members')): the_row() ?>
+							<li>
+								<p><?php echo get_sub_field('name') ?></p>
+								<p><?php echo get_sub_field('title') ?></p>
+						
+							</li>
+						<?php endwhile; ?>
+					</ul>
+				<?php endif; ?>
+
+			<?php endif; ?>
+
+
+
+			
+
+		<?php endwhile; // End of the loop.?>
+
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
